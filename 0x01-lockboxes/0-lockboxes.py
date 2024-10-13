42416d9d248a5981ef45a83c0632fbb726d06afd
Devepiphany1 <franklinanthony89@>
2024-10-13 17:37:28 -0600

    Updatde

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 def can_unlock_all(boxes):
-        """Determines if all boxes can be unlocked."""
-            n = len(boxes)
-                opened = {0}  # Start with the first box already opened
-                    stack = [0]  # Stack to manage the boxes to be explored
+"""Determines if all boxes can be unlocked."""
+    n = len(boxes)
+    opened = {0}  # Start with the first box already opened
+    stack = [0]  # Stack to manage the boxes to be explored
 
-                        while stack:
-                                    box = stack.pop()
+    while stack:
+        box = stack.pop()
 
-                                            for key in boxes[box]:
-                                                            if key < n and key not in opened:
-                                                                                opened.add(key)
-                                                                                                stack.append(key)
+    for key in boxes[box]:
+        if key < n and key not in opened:
+            opened.add(key)
+            stack.append(key)
 
-                                                                                                    return len(opened) == n
+return len(opened) == n
                                                                                             
